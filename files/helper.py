410909f315665644cce81972be7edef8a3a8ba23
Matheus Cerqueira <matheus.crqra@gmail.com>
2023-04-16 22:30:10 -0300

    Remove unused logic

--- files/helper.py
+++ files/helper.py
@@ -269,22 +269,32 @@ def __conv(cube_verts):
     return new_verts
 
 # Retorna os vértices do cubo
+
+
 def get_cube_verts():
     return __conv(__cube_verts_lim)
 
 # Retorna as peças de centro
+
+
 def get_center_pieces():
     return [__conv(piece) for piece in __center_pieces]
 
 # Retorna as peças de canto
+
+
 def get_corner_pieces():
     return [__conv(piece) for piece in __corner_pieces]
 
 # Retorna as peças intermediárias
+
+
 def get_edge_pieces():
     return [[__conv(sec_piece) for sec_piece in piece] for piece in __edge_pieces]
 
 # Retorna as cores
+
+
 def get_cube_colors():
     return [
         (0.0, 0.318, 0.729),    # Blue
@@ -295,90 +305,11 @@ def get_cube_colors():
         (1.0, 0.85, 0.1)        # Yellow
     ]
 
-# Rotaciona uma coluna do cubo
-def __rot_column(a, b, left=False):
-    c = []
-
-    for i in range(len(a)):
-        row = []
-
-        if not left:
-            row.append(a[i][0])
-            row.append(a[i][1])
-            row.append(b[i][2])
-
-        else:
-            row.append(b[i][0])
-            row.append(a[i][1])
-            row.append(a[i][2])
-
-        c.append(row)
-
-    return c
-
 
 class Cube:
     def __init__(self, cubes):
         self.cubes = cubes
         self.original = cubes.copy()
-
-    def __rot_t(self):
-        temp = self.cubes[1][0]
-        self.cubes[1][0] = self.cubes[2][0][::-1]
-        self.cubes[2][0] = self.cubes[3][0][::-1]
-        self.cubes[3][0] = self.cubes[4][2]
-        self.cubes[4][0] = temp
-
-    def __rot_r(self):
-        # Rotate the right face anti-clockwise
-        temp = self.cubes[0]
-        self.cubes[0] = __rot_column(self.cubes[0], self.cubes[3])
-        self.cubes[3] = __rot_column(self.cubes[3], self.cubes[5])
-        self.cubes[5] = __rot_column(self.cubes[5], self.cubes[1])
-        self.cubes[1] = __rot_column(self.cubes[1], temp)
-
-    def __rot_f(self):
-        # Rotate the front face anti-clockwise
-        temp = self.cubes[0][2]
-        self.cubes[0][2] = self.cubes[2][2][::-1]
-        self.cubes[2][2] = self.cubes[5][2]
-        self.cubes[5][2] = self.cubes[3][0][::-1]
-        self.cubes[3][0] = temp
-
-    def __rot_l(self):
-        # Rotate the left face anti-clockwise
-        temp = self.cubes[0]
-        self.cubes[0] = __rot_column(
-            self.cubes[0], self.cubes[3], left=True)
-        self.cubes[3] = __rot_column(
-            self.cubes[3], self.cubes[5], left=True)
-        self.cubes[5] = __rot_column(
-            self.cubes[5], self.cubes[1], left=True)
-        self.cubes[1] = __rot_column(self.cubes[1], temp, left=True)
-
-    def __rot_b(self):
-        # Rotate the bottom face anti-clockwise
-        self.cubes[5] = [list(x) for x in self.cubes[5][::-1]]
-        temp = self.cubes[1][2]
-        self.cubes[1][2] = self.cubes[2][2][::-1]
-        self.cubes[2][2] = self.cubes[3][2][::-1]
-        self.cubes[3][2] = self.cubes[4][0]
-        self.cubes[4][2] = temp
-
-    def rotate_face(self, face):
-        # Rotate the specified face
-        if face == 'top':
-            self.__rot_t()
-        elif face == 'right':
-            self.__rot_r()
-        elif face == 'front':
-            self.__rot_f()
-        elif face == 'left':
-            self.__rot_l()
-        elif face == 'bottom':
-            self.__rot_b()
-        else:
-            raise ValueError('Invalid face')
 
     def __str__(self):
 
@@ -389,3 +320,61 @@ class Cube:
             ret = ret + '\n'
 
         return ret
+
+    def get_surface_cubes(self, face):
+        """
+        Return surfaces of a cube
+        @param face: 'top', 'right', 'left', 'bottom', 'all'
+        """
+
+        sur_cubes = []
+
+        if face == 'top':
+            for row in self.cubes[0]:
+                for c in row:
+                    sur_cubes.append(c)
+            sur_cubes.extend(self.cubes[1][0])
+            sur_cubes.extend(self.cubes[2][0])
+            sur_cubes.extend(self.cubes[3][0])
+            sur_cubes.extend(self.cubes[4][0])
+            return sur_cubes
+
+        elif face == 'right':
+            for row in self.cubes[2]:
+                for c in row:
+                    sur_cubes.append(c)
+            sur_cubes.extend([row[2] for row in self.cubes[0]])
+            sur_cubes.extend([row[2] for row in self.cubes[1]])
+            sur_cubes.extend([row[2] for row in self.cubes[3]])
+            sur_cubes.extend([row[2] for row in self.cubes[5]])
+            return sur_cubes
+
+        elif face == 'left':
+            for row in self.cubes[4]:
+                for c in row:
+                    sur_cubes.append(c)
+            sur_cubes.extend([row[0] for row in self.cubes[0]])
+            sur_cubes.extend([row[0] for row in self.cubes[1]])
+            sur_cubes.extend([row[0] for row in self.cubes[3]])
+            sur_cubes.extend([row[0] for row in self.cubes[5]])
+            return sur_cubes
+
+        elif face == 'bottom':
+            for row in self.cubes[5]:
+                for c in row:
+                    sur_cubes.append(c)
+            sur_cubes.extend(self.cubes[1][2])
+            sur_cubes.extend(self.cubes[2][2])
+            sur_cubes.extend(self.cubes[3][2])
+            sur_cubes.extend(self.cubes[4][2])
+            return sur_cubes
+
+        elif face == 'all':
+            for surface in self.cubes:
+                for row in surface:
+                    for c in row:
+                        sur_cubes.append(c)
+            return sur_cubes
+
+        else:
+            raise ValueError('Invalid face')
